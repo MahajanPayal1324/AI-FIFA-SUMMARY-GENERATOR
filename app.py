7910cedf6ce8c29e2a0fa56ae439b193b397210a
Payal Mahajan <mahajanpayal1304@gmail.com>
2026-07-09 22:04:20 +0530

    Connect streamlit UI with backend that is match retrieval and AI summarization

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+from api.football_api import find_match
+from llm.summarizer import generate_summary
+
 st.set_page_config(
     page_title="AI FIFA Match Summary Generator",
     page_icon="⚽"
@@ -7,9 +10,7 @@ st.set_page_config(
 
 st.title("⚽ AI FIFA Match Summary Generator")
 
-st.write(
-    "Generate AI-powered summaries for FIFA World Cup matches."
-)
+st.write("Generate AI-powered summaries for FIFA World Cup matches.")
 
 st.divider()
 
@@ -17,4 +18,23 @@ team = st.text_input("Enter Team Name")
 
 match_date = st.date_input("Select Match Date")
 
-st.button("Generate Summary")
+if st.button("Generate Summary"):
+
+    match = find_match(
+        team,
+        str(match_date)
+    )
+
+    if match:
+
+        with st.spinner("Generating AI Summary..."):
+
+            summary = generate_summary(match)
+
+        st.success("Summary Generated!")
+
+        st.write(summary)
+
+    else:
+
+        st.error("Match not found.")
