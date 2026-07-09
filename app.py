3151dd9a5434de79a94855b4338c53c88aaa1507
Payal Mahajan <mahajanpayal1304@gmail.com>
2026-07-09 21:43:22 +0530

    Made Country and Date calender feature to make easier user experience.

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ st.set_page_config(
     page_icon="⚽"
 )
 
-st.title("⚽ AI Powered FIFA Match Summary Generator")
+st.title("⚽ AI FIFA Match Summary Generator")
 
 st.write(
     "Generate AI-powered summaries for FIFA World Cup matches."
 )
+
+st.divider()
+
+team = st.text_input("Enter Team Name")
+
+match_date = st.date_input("Select Match Date")
+
+st.button("Generate Summary")
